c7481393e662ca5b5bcb627d15d5ad0df59ccf6e
Sidney <sidney.croset@hotmail.com>
2024-02-06 17:32:19 +0100

    CSS modifications

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const cells = document.querySelectorAll(".cell");
 let currentPlayer = "X";
+let board = [];
 
 cells.forEach((cell) => {
   cell.addEventListener("click", function (event) {
     if (!event.target.textContent) {
       event.target.textContent = currentPlayer;
-      event.target.classList.add(currentPlayer); 
+      event.target.classList.add(currentPlayer);
       currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
   });
 });
+
+
