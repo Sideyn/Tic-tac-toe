f8a482fdcd67ab965341e6cb792b99e6779a9a17
Sidney <sidney.croset@hotmail.com>
2024-02-06 15:51:36 +0100

    CSS modifications and add colors

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ cells.forEach((cell) => {
   cell.addEventListener("click", function (event) {
     if (!event.target.textContent) {
       event.target.textContent = currentPlayer;
+      event.target.classList.add(currentPlayer); 
       currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
   });
